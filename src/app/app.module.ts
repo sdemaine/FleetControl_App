9ba350df3f9487644173c68cf5694a438031b644
Stephen Demaine <stephendemaine@gmail.com>
2018-01-21 20:02:12 -0500

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppNavbarComponent } from './navigation/app-navbar/navbar.component';
+import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
+import { PageNotFoundComponent} from './navigation/page-not-found/page-not-found.component';
+import { ProductGlossaryComponent } from './product/product-glossary/product-glossary.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+
+// routing - this needs to be moved to a separate file
+const appRoutes: Routes = [
+  { path: 'inventory-list', component: InventoryListComponent },
+  { path: 'product-glossary', component: ProductGlossaryComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component:  PageNotFoundComponent}
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent,
+    InventoryListComponent,
+    PageNotFoundComponent,
+    ProductGlossaryComponent,
+    ShoppingCartComponent,
+    ContactUsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: false} // for debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/product/product-glossary/product-glossary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-glossary',
+  templateUrl: './product-glossary.component.html',
+  styleUrls: ['./product-glossary.component.scss']
+})
+export class ProductGlossaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
